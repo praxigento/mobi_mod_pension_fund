763f3dd0567ffddffcce016e193445cb3176c91d
Alex Gusev <alex@flancer64.com>
2018-01-24 20:58:12 +0200

    MOBI-691 Add "Processing fee" service

--- Service/Collect/Fee.php
+++ Service/Collect/Fee.php
@@ -5,6 +5,7 @@
 
 namespace Praxigento\PensionFund\Service\Collect;
 
+use Praxigento\PensionFund\Config as Cfg;
 use Praxigento\PensionFund\Service\Collect\Fee\Request as ARequest;
 use Praxigento\PensionFund\Service\Collect\Fee\Response as AResponse;
 
@@ -30,10 +31,30 @@ class Fee
         assert($request instanceof ARequest);
 
         /** perform processing */
+        list($feePeriodData, $feeCalcData) = $this->getCalcData();
 
         /** compose result */
         $result = new AResponse();
         return $result;
     }
 
+    /**
+     * Get data for dependent calculation.
+     *
+     * @return array [$periodData, $calcData]
+     */
+    private function getCalcData()
+    {
+        /* get period & calc data */
+        $req = new \Praxigento\BonusBase\Api\Service\Period\Calc\Get\Dependent\Request();
+        $req->setBaseCalcTypeCode(Cfg::CODE_TYPE_CALC_BONUS_INFINITY_EU);
+        $req->setDepCalcTypeCode(Cfg::CODE_TYPE_CALC_PROC_FEE);
+        $resp = $this->servCalcDep->exec($req);
+        /** @var \Praxigento\BonusBase\Repo\Entity\Data\Period $periodData */
+        $periodData = $resp->getDepPeriodData();
+        /** @var \Praxigento\BonusBase\Repo\Entity\Data\Calculation $calcData */
+        $calcData = $resp->getDepCalcData();
+        $result = [$periodData, $calcData];
+        return $result;
+    }
 }

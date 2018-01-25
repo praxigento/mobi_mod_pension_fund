44186ead554c52d7975405677b7f7b983719e09a
Alex Gusev <alex@flancer64.com>
2018-01-25 19:10:43 +0200

    MOBI-691 Add "Processing fee" service

--- Service/Collect/Fee.php
+++ Service/Collect/Fee.php
@@ -5,19 +5,37 @@
 
 namespace Praxigento\PensionFund\Service\Collect;
 
+use Praxigento\Accounting\Repo\Entity\Data\Operation as EOper;
 use Praxigento\PensionFund\Config as Cfg;
+use Praxigento\PensionFund\Service\Collect\Fee\Own\Repo\Query\GetCreditTotals as QBGetCreditTotals;
 use Praxigento\PensionFund\Service\Collect\Fee\Request as ARequest;
 use Praxigento\PensionFund\Service\Collect\Fee\Response as AResponse;
 
 class Fee
 {
+    /** @var \Praxigento\Core\Api\Helper\Period */
+    private $hlpPeriod;
+    /** @var \Praxigento\PensionFund\Service\Collect\Fee\Own\Repo\Query\GetCreditTotals */
+    private $qbGetCreditTotals;
+    /** @var \Praxigento\Accounting\Repo\Entity\Type\Asset */
+    private $repoTypeAsset;
+    /** @var \Praxigento\Accounting\Repo\Entity\Type\Operation */
+    private $repoTypeOper;
     /** @var \Praxigento\BonusBase\Api\Service\Period\Calc\Get\Dependent */
     private $servCalcDep;
 
     public function __construct(
-        \Praxigento\BonusBase\Api\Service\Period\Calc\Get\Dependent $servCalcDep
+        \Praxigento\Accounting\Repo\Entity\Type\Asset $repoTypeAsset,
+        \Praxigento\Accounting\Repo\Entity\Type\Operation $repoTypeOper,
+        \Praxigento\Core\Api\Helper\Period $hlpPeriod,
+        \Praxigento\BonusBase\Api\Service\Period\Calc\Get\Dependent $servCalcDep,
+        QBGetCreditTotals $qbGetCreditTotals
     ) {
+        $this->repoTypeAsset = $repoTypeAsset;
+        $this->repoTypeOper = $repoTypeOper;
+        $this->hlpPeriod = $hlpPeriod;
         $this->servCalcDep = $servCalcDep;
+        $this->qbGetCreditTotals = $qbGetCreditTotals;
     }
 
 
@@ -31,8 +49,15 @@ class Fee
         assert($request instanceof ARequest);
 
         /** perform processing */
+        /**
+         * @var \Praxigento\BonusBase\Repo\Entity\Data\Period $feePeriodData
+         * @var \Praxigento\BonusBase\Repo\Entity\Data\Calculation $feeCalcData
+         */
         list($feePeriodData, $feeCalcData) = $this->getCalcData();
+        $dsBegin = $feePeriodData->getDstampBegin();
+        $dsEnd = $feePeriodData->getDstampEnd();
 
+        $bu = $this->getCreditTotal($dsBegin, $dsEnd);
         /** compose result */
         $result = new AResponse();
         return $result;
@@ -55,6 +80,58 @@ class Fee
         /** @var \Praxigento\BonusBase\Repo\Entity\Data\Calculation $calcData */
         $calcData = $resp->getDepCalcData();
         $result = [$periodData, $calcData];
+        return $result;
+    }
+
+    private function getCreditTotal($dsFrom, $dsTo)
+    {
+        $dateFrom = $this->hlpPeriod->getTimestampFrom($dsFrom);
+        $dateTo = $this->hlpPeriod->getTimestampNextFrom($dsTo);
+        $assetWallet = $this->repoTypeAsset->getIdByCode(Cfg::CODE_TYPE_ASSET_WALLET);
+        $operTypeIds = $this->getOperTypesApplied();
+
+        /* compose query */
+        $query = $this->qbGetCreditTotals->build();
+        $byOperType = '(0)';
+        foreach ($operTypeIds as $typeId) {
+            $cond = QBGetCreditTotals::AS_OPER . '.' . EOper::ATTR_TYPE_ID . '=' . (int)$typeId;
+            $byOperType .= " OR ($cond)";
+        }
+        $query->where($byOperType);
+        $conn = $query->getConnection();
+        $bind = [
+            QBGetCreditTotals::BND_DATE_FROM => $dateFrom,
+            QBGetCreditTotals::BND_DATE_TO => $dateTo,
+            QBGetCreditTotals::BND_ASSET_TYPE_ID => $assetWallet
+        ];
+        $rs = $conn->fetchAll($query, $bind);
+        $result = [];
+        foreach ($rs as $one) {
+            $custId = $one[QBGetCreditTotals::A_CUST_ID];
+            $total = $one[QBGetCreditTotals::A_CREDIT];
+            if (isset($result[$custId])) {
+                throw  new \Magento\Framework\Exception\LocalizedException("Processing fee: two credits for one customer");
+            } else {
+                $result[$custId] = $total;
+            }
+        }
+    }
+
+    /**
+     * Get IDs for operation types allowed for fee processing.
+     *
+     * @return int[]
+     */
+    private function getOperTypesApplied()
+    {
+        $result = [];
+        $result[] = $this->repoTypeOper->getIdByCode(Cfg::CODE_TYPE_OPER_BONUS_COURTESY);
+        $result[] = $this->repoTypeOper->getIdByCode(Cfg::CODE_TYPE_OPER_BONUS_INFINITY);
+        $result[] = $this->repoTypeOper->getIdByCode(Cfg::CODE_TYPE_OPER_BONUS_OVERRIDE);
+        $result[] = $this->repoTypeOper->getIdByCode(Cfg::CODE_TYPE_OPER_BONUS_PERSONAL);
+        $result[] = $this->repoTypeOper->getIdByCode(Cfg::CODE_TYPE_OPER_BONUS_REBATE);
+        $result[] = $this->repoTypeOper->getIdByCode(Cfg::CODE_TYPE_OPER_BONUS_SIGNUP_DEBIT);
+        $result[] = $this->repoTypeOper->getIdByCode(Cfg::CODE_TYPE_OPER_BONUS_TEAM);
         return $result;
     }
 }
---
+++ Service/Collect/Fee/Own/Repo/Query/GetCreditTotals.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\PensionFund\Service\Collect\Fee\Own\Repo\Query;
+
+use Praxigento\Accounting\Repo\Entity\Data\Account as EAcc;
+use Praxigento\Accounting\Repo\Entity\Data\Operation as EOper;
+use Praxigento\Accounting\Repo\Entity\Data\Transaction as ETrans;
+use Praxigento\Core\App\Repo\Query\Expression as AnExpression;
+
+class GetCreditTotals
+    extends \Praxigento\Core\App\Repo\Query\Builder
+{
+
+    /** Tables aliases for external usage ('camelCase' naming) */
+    const AS_ACC = 'acc';
+    const AS_OPER = 'oper';
+    const AS_TRANS = 'trans';
+
+    /** Columns/expressions aliases for external usage ('camelCase' naming) */
+    const A_CREDIT = 'credit';
+    const A_CUST_ID = 'custId';
+
+    /** Bound variables names ('camelCase' naming) */
+    const BND_ASSET_TYPE_ID = 'assetTypeId';
+    const BND_DATE_FROM = 'dateFrom';       // inclusive
+    const BND_DATE_TO = 'dateTo';           // exclusive
+
+    /** Entities are used in the query */
+    const E_ACC = EAcc::ENTITY_NAME;
+    const E_OPER = EOper::ENTITY_NAME;
+    const E_TRANS = ETrans::ENTITY_NAME;
+
+    public function build(\Magento\Framework\DB\Select $source = null)
+    {
+
+        /* this is root query builder (started from SELECT) */
+        $result = $this->conn->select();
+
+        /* define tables aliases for internal usage (in this method) */
+        $asAcc = self::AS_ACC;
+        $asOper = self::AS_OPER;
+        $asTrans = self::AS_TRANS;
+
+        /* FROM prxgt_acc_transaction */
+        $tbl = $this->resource->getTableName(self::E_TRANS);
+        $as = $asTrans;
+        $exp = "SUM($asTrans." . ETrans::ATTR_VALUE . ")";
+        $exp = new AnExpression($exp);
+        $cols = [
+            self::A_CREDIT => $exp
+        ];
+        $result->from([$as => $tbl], $cols);
+
+        /* LEFT JOIN prxgt_acc_account (to get customer ID) */
+        $tbl = $this->resource->getTableName(self::E_ACC);
+        $as = $asAcc;
+        $cols = [
+            self::A_CUST_ID => EAcc::ATTR_CUST_ID
+        ];
+        $cond = $as . '.' . EAcc::ATTR_ID . '=' . $asTrans . '.' . ETrans::ATTR_CREDIT_ACC_ID;
+        $result->joinLeft([$as => $tbl], $cond, $cols);
+
+        /* LEFT JOIN prxgt_acc_operation (to apply filters by operation type) */
+        $tbl = $this->resource->getTableName(self::E_OPER);
+        $as = $asOper;
+        $cols = [];
+        $cond = $as . '.' . EOper::ATTR_ID . '=' . $asTrans . '.' . ETrans::ATTR_OPERATION_ID;
+        $result->joinLeft([$as => $tbl], $cond, $cols);
+
+        /* query tuning */
+        $result->group($asTrans . '.' . ETrans::ATTR_CREDIT_ACC_ID);
+        $byFrom = "$asTrans." . ETrans::ATTR_DATE_APPLIED . ">=:" . self::BND_DATE_FROM;
+        $byTo = "$asTrans." . ETrans::ATTR_DATE_APPLIED . "<:" . self::BND_DATE_TO;
+        $byAssetType = "$asAcc." . EAcc::ATTR_ASSET_TYPE_ID . "=:" . self::BND_ASSET_TYPE_ID;
+        $result->where("($byFrom) AND ($byTo) AND ($byAssetType)");
+
+        return $result;
+    }
+}

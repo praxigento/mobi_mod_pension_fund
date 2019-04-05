b528a462c428d7e7810662102df85ccb2f3b7bdd
Alex Gusev <alex@flancer64.com>
2019-04-05 15:04:15 +0300

    SAN-614 Investigate bonus calcs on test

--- Cli/Collect/PensionAssets.php
+++ Cli/Collect/PensionAssets.php
@@ -11,53 +11,28 @@ namespace Praxigento\PensionFund\Cli\Collect;
 class PensionAssets
     extends \Praxigento\Core\App\Cli\Cmd\Base
 {
-    /** @var \Magento\Framework\DB\Adapter\AdapterInterface */
-    private $conn;
-    /** @var \Magento\Framework\App\ResourceConnection */
-    private $resource;
     /** @var \Praxigento\PensionFund\Service\Collect\Assets */
     private $servCollectAssets;
 
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $manObj,
-        \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\PensionFund\Service\Collect\Assets $servCollectAssets
     ) {
         parent::__construct(
-            $manObj,
             'prxgt:pension:collect:assets',
             'Collect pension related data and add assets to pension accounts for the last calculated period.'
         );
-        $this->resource = $resource;
-        $this->conn = $resource->getConnection();
         $this->servCollectAssets = $servCollectAssets;
     }
 
-    protected function execute(
-        \Symfony\Component\Console\Input\InputInterface $input,
-        \Symfony\Component\Console\Output\OutputInterface $output
-    ) {
-        $output->writeln("<info>Command '" . $this->getName() . "' is started.<info>");
-        /* wrap all DB operations with DB transaction */
-        $this->conn->beginTransaction();
-        try {
-            $req = new \Praxigento\PensionFund\Service\Collect\Assets\Request();
-            $resp = $this->servCollectAssets->exec($req);
-            $operIdIncome = $resp->getOperIdIncome();
-            $operIdPercent = $resp->getOperIdPercent();
-            $operIdReturn = $resp->getOperIdReturn();
-            $operIdCleanup = $resp->getOperIdCleanup();
-            $msg = "(income: #$operIdIncome, percent: #$operIdPercent, return: #$operIdReturn, cleanup: #$operIdCleanup)";
-            $output->writeln(
-                "<info>Pension assets processing operations $msg are created.<info>"
-            );
-
-            $this->conn->commit();
-        } catch (\Throwable $e) {
-            $output->writeln('<info>Command \'' . $this->getName() . '\' failed. Reason: '
-                . $e->getMessage() . '.<info>');
-            $this->conn->rollBack();
-        }
-        $output->writeln('<info>Command \'' . $this->getName() . '\' is completed.<info>');
+    protected function process(\Symfony\Component\Console\Input\InputInterface $input)
+    {
+        $req = new \Praxigento\PensionFund\Service\Collect\Assets\Request();
+        $resp = $this->servCollectAssets->exec($req);
+        $operIdIncome = $resp->getOperIdIncome();
+        $operIdPercent = $resp->getOperIdPercent();
+        $operIdReturn = $resp->getOperIdReturn();
+        $operIdCleanup = $resp->getOperIdCleanup();
+        $msg = "(income: #$operIdIncome, percent: #$operIdPercent, return: #$operIdReturn, cleanup: #$operIdCleanup)";
+        $this->logInfo("Pension assets processing operations $msg are created.");
     }
 }
--- Cli/Collect/ProcessingFee.php
+++ Cli/Collect/ProcessingFee.php
@@ -11,48 +11,25 @@ namespace Praxigento\PensionFund\Cli\Collect;
 class ProcessingFee
     extends \Praxigento\Core\App\Cli\Cmd\Base
 {
-    /** @var \Magento\Framework\DB\Adapter\AdapterInterface */
-    private $conn;
-    /** @var \Magento\Framework\App\ResourceConnection */
-    private $resource;
     /** @var \Praxigento\PensionFund\Service\Collect\Fee */
     private $servCollectFee;
 
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $manObj,
-        \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\PensionFund\Service\Collect\Fee $servCollectFee
     ) {
         parent::__construct(
-            $manObj,
             'prxgt:pension:collect:fee',
             'Collect processing fee for the last calculated period.'
         );
-        $this->resource = $resource;
-        $this->conn = $resource->getConnection();
         $this->servCollectFee = $servCollectFee;
     }
 
-    protected function execute(
-        \Symfony\Component\Console\Input\InputInterface $input,
-        \Symfony\Component\Console\Output\OutputInterface $output
-    ) {
-        $output->writeln("<info>Start processing fee collection.<info>");
-        /* wrap all DB operations with DB transaction */
-        $this->conn->beginTransaction();
-        try {
-
+    protected function process(\Symfony\Component\Console\Input\InputInterface $input)
+    {
         $req = new \Praxigento\PensionFund\Service\Collect\Fee\Request();
         $resp = $this->servCollectFee->exec($req);
         $operId = $resp->getOperationId();
-        $output->writeln("<info>Processing fee operation #$operId is created.<info>");
+        $this->logInfo("Processing fee operation #$operId is created.");
 
-            $this->conn->commit();
-        } catch (\Throwable $e) {
-            $output->writeln('<info>Command \'' . $this->getName() . '\' failed. Reason: '
-                . $e->getMessage() . '.<info>');
-            $this->conn->rollBack();
-        }
-        $output->writeln('<info>Command \'' . $this->getName() . '\' is completed.<info>');
     }
 }

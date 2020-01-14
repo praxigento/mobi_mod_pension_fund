16b370083b0096a709ea6ec7d618e3835bbdd729
Alex Gusev <alex@flancer64.com>
2020-01-14 12:10:42 +0200

    SAN-762 Pension program months total counter

--- Config.php
+++ Config.php
@@ -7,9 +7,6 @@
 
 namespace Praxigento\PensionFund;
 
-use Praxigento\Accounting\Config as AccCfg;
-use Praxigento\BonusReferral\Config as BonRefCfg;
-use Praxigento\Pv\Config as PvCfg;
 use Praxigento\Wallet\Config as WalletCfg;
 
 class Config
@@ -23,16 +20,12 @@ class Config
     const CODE_TYPE_CALC_PENSION = 'PENSION';
     const CODE_TYPE_CALC_PROC_FEE = 'PROC_FEE';
 
-    const CODE_TYPE_OPER_BONUS_REF_BOUNTY = BonRefCfg::CODE_TYPE_OPER_BONUS_REF_BOUNTY;
-    const CODE_TYPE_OPER_BONUS_REF_FEE = BonRefCfg::CODE_TYPE_OPER_BONUS_REF_FEE;
-    const CODE_TYPE_OPER_PENSION_INCOME = 'PENSION_INCOME';
     const CODE_TYPE_OPER_PENSION_CLEANUP = 'PENSION_CLEANUP';
+    const CODE_TYPE_OPER_PENSION_INCOME = 'PENSION_INCOME';
     const CODE_TYPE_OPER_PENSION_PERCENT = 'PENSION_PERCENT';
     const CODE_TYPE_OPER_PENSION_RETURN = 'PENSION_RETURN';
     const CODE_TYPE_OPER_PROC_FEE_DEF = 'PROC_FEE_DEF';
     const CODE_TYPE_OPER_PROC_FEE_EU = 'PROC_FEE_EU';
-    const CODE_TYPE_OPER_PV_TRANSFER = PvCfg::CODE_TYPE_OPER_PV_TRANSFER;
-    const CODE_TYPE_OPER_WALLET_SALE = WalletCfg::CODE_TYPE_OPER_WALLET_SALE;
 
     const DEF_PENSION_INTEREST_PERCENT = 0.03;
     const MENU_PENSION_FUND = self::ACL_PENSION_FUND;

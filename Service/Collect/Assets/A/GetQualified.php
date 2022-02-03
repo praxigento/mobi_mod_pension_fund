540d0be30226d4c71967126fa46f6b9848f3e93b
Alex Gusev <alex@flancer64.com>
2022-02-03 13:22:38 +0200

    SAN-1023 Add DAINA LAZDANE to eternal pensionaries

--- Service/Collect/Assets/A/GetQualified.php
+++ Service/Collect/Assets/A/GetQualified.php
@@ -73,7 +73,6 @@ class GetQualified
     {
         $result = [];
         $where = EDwnlCust::A_MLM_ID . '="777038763"';
-        $where .= ' OR ' . EDwnlCust::A_MLM_ID . '="777104780"';
         $where .= ' OR ' . EDwnlCust::A_MLM_ID . '="100002146"';
         $rs = $this->daoDwnlCust->get($where);
         /** @var EDwnlCust $one */
